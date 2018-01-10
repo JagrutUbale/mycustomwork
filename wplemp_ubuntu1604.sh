1740a33f47f0a253b169cae373eb9a379e653ab8
JagrutUbale <jagrutubale@gmail.com>
2018-01-10 19:30:43 +0530

    Update and rename wplempU16x.sh to wplemp_ubuntu1604.sh

--- wplemp_ubuntu1604.sh
+++ wplemp_ubuntu1604.sh
@@ -12,7 +12,7 @@ touch /tmp/tasks
 apt-get update >> /tmp/tasks
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"  
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root" 
-packages="mysql-server nginx php php-mysql php-fpm nano wget curl ed elinks tar" 
+packages="mysql-server nginx php php-mysql php-fpm php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc nano wget curl ed elinks tar sudo adduser" 
 for pkg in $packages; do
 	dpkg -s $pkg &>> /tmp/tasks
 	if [ $? -eq 0 ]; then
@@ -24,32 +24,48 @@ for pkg in $packages; do
 done
 service mysql restart >> /tmp/tasks
 mysql --user=root --password=root -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress'; FLUSH PRIVILEGES;" >> /tmp/tasks
-
 mkdir -p "/var/www/$1"  
-echo "127.0.0.1 $1" >> /etc/hosts  
+echo "127.0.0.1 $1" >> /etc/hosts
 cat <<EOF> /etc/nginx/sites-available/$1
 server {
-        listen   80;
+        listen   80 default_server;
         root /var/www/$1;
         index index.php index.html index.htm;
-        server_name "$1";
+        server_name _;
         location / {
                 try_files \$uri \$uri/ /index.php?q=\$uri&\$args;
+        }
+        location = /favicon.ico {
+                log_not_found off;
+                access_log off;
+        }
+        location = /robots.txt {
+                allow all;
+                log_not_found off;
+                access_log off;
+        }
+        location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
+                expires max;
+                log_not_found off;
         }
         error_page 404 /404.html;
         error_page 500 502 503 504 /50x.html;
         location = /50x.html {
-              root /usr/share/nginx/www;
+              root /var/www/$1;
         }
         location ~ \.php$ {
                 try_files \$uri =404;
-                fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+                fastcgi_pass unix:/run/php/php7.0-fpm.sock;
                 fastcgi_index index.php;
+                fastcgi_param SCRIPT_FILENAME $document_root/$fastcgi_script_name;
                 include fastcgi_params;
-                 } 
+        } 
 }
 EOF
 ln -sf /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
+rm /etc/nginx/sites-enabled/default
+chown -R www-data:www-data /var/www/$1
+chmod -R g+s /var/www/$1
 cd /var/www/
 wget http://wordpress.org/latest.tar.gz >> /tmp/tasks
 tar -xvf latest.tar.gz >> /tmp/tasks
@@ -59,13 +75,13 @@ mv wp-config-sample.php wp-config.php
 sed -i "s/username_here/wordpress/"  wp-config.php >> /tmp/tasks
 sed -i "s/database_name_here/wordpress/"  wp-config.php >> /tmp/tasks
 sed -i "s/password_here/wordpress/"   wp-config.php >> /tmp/tasks
+echo "define('FS_METHOD', 'direct');" >> wp-config.php 
 SALT=$(curl -s -L https://api.wordpress.org/secret-key/1.1/salt/)
-STRING='put your unique phrase here'
-printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s wp-config.php
+printf '%s\n' "g/put your unique phrase here/d" a "$SALT" . w | ed -s wp-config.php
 chown -R www-data:www-data /var/www/$1
 chmod -R 755 /var/www/$1
-
-service nginx restart  >> /tmp/tasks
-/etc/init.d/php7.0-fpm restart  >> /tmp/tasks
-service mysql restart  >> /tmp/tasks
+sed -i -e "s/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini
+sudo service nginx restart  >> /tmp/tasks
+sudo service php7.0-fpm restart  >> /tmp/tasks
+sudo service mysql restart  >> /tmp/tasks
 elinks http://$1 
